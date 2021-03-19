fdc61c77ad7c2c9cf92d403303839da2a759a3c6
Fothul Karim Forhan <fothulkforhan@gmail.com>
2021-03-19 15:28:03 +0200

    Feedback and Question1 added

--- src/main/java/ee/taltech/calculator/theory/a_theory2/question1/Gradle.java
+++ src/main/java/ee/taltech/calculator/theory/a_theory2/question1/Gradle.java
@@ -8,20 +8,23 @@ public class Gradle {
     //todo A
     // In your words (do not copy wiki definitions)
     // What is gradle?
-    // Answer:
+    // Answer: Gradle is a build automation tool,which is open source and can be used to make projects quickly
+    // and through automating.
 
     //todo B
     // What is gradle wrapper? Why are we using it instead of gradle?
-    // Answer:
+    // Answer: Gradle wrapper is script which is added to Gradle project and its used to execute the build.
+    // We are using it instead of gradle because we don't need to have gradle installed in the machine if we
+    // use gradle wrapper , It also guarantees the exect version we need to work on our project.
 
     //todo C
     // Name 3 different benefits or reasons for using gradle
-    // 1
-    // 2
-    // 3
+    // 1 Open Source
+    // 2 High Performance
+    // 3 Flexibility (Plug in based build tool)
 
     //todo D
     // Name at least 1 alternative to gradle
-    // 1
+    // 1 Apache Maven
 
 }
--- src/main/java/ee/taltech/calculator/theory/a_theory2/question3contribution/Feedback2.java
+++ src/main/java/ee/taltech/calculator/theory/a_theory2/question3contribution/Feedback2.java
@@ -10,21 +10,23 @@ public class Feedback2 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 8
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
+    // I liked the detailed explanation about everything.
+    // I liked all the lecture's and how they are seperated into different small videos.
+    // I loved the humours too.
     //
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
+    // The only thing I didn't like is that I wasn't punctual enough.
     //
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // Nope. Its alright.
     //
 
 }
