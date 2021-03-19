1bccebc236f7598e7f7ededdfa49af45b32b3ec0
Mehmet Secgin <mesecg@ttu.ee>
2021-03-19 16:30:32 +0300

    Feedback done

--- src/main/java/ee/taltech/calculator/theory/a_theory2/question3contribution/Feedback1.java
+++ src/main/java/ee/taltech/calculator/theory/a_theory2/question3contribution/Feedback1.java
@@ -7,24 +7,32 @@ public class Feedback1 {
 
     // todo
     //  The course is coming to an end and I would like you to tell me how did it go
+    //  *
+    //  It was awesome. I feel like I've learned a LOT and even got inspired to do some other project that others
+    //  will benefit too. I've met IntelliJ and Pycharm, cant even use other IDEs. I've learnt about git and also
+    //  started using it on my own projects too.
 
-    //todo A
-    // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
+    // todo A
+    //  On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
+    //  *
+    //  10 without a doubt.
+
+    // todo B
+    //  Write some things you liked? At least 1, but you can write more.
+    //  *
+    //  I like the way you're teaching a lot. Can say that I didnt get bored in any lessons.
+    //  Felt like watching tutorials on YouTube(That was a compliment :) ).
     //
 
-    //todo B
-    // Write some things you liked? At least 1, but you can write more.
+    // todo C
+    //  Write some things you didn't like? At least 1, but you can write more.
     //
     //
 
-    //todo C
-    // Write some things you didn't like? At least 1, but you can write more.
-    //
-    //
-
-    //todo D
-    // Anything else to add? Comments? What to do differently next year? (optional)
-    //
-    //
+    // todo D
+    //  Anything else to add? Comments? What to do differently next year? (optional)
+    //  *
+    //  I liked that the videos were in parts, it was so easy to follow and find materials.
+    //  I dont know how would it be if it wasn't for covid situations.
 
 }
