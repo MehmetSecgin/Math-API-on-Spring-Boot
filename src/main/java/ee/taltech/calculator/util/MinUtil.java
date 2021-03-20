5461455365f9e9d5fa1d3811a4d7daa27dc83cc8
Mehmet Secgin <mesecg@ttu.ee>
2021-03-20 17:02:34 +0300

    fixed min for null input

--- src/main/java/ee/taltech/calculator/util/MinUtil.java
+++ src/main/java/ee/taltech/calculator/util/MinUtil.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class MinUtil {
 
     public static Integer min(List<Integer> input) {
+        if (input.isEmpty())
+            return null;
         return input.stream()
                 .min(Comparator.comparing(Integer::valueOf))
                 .get();
