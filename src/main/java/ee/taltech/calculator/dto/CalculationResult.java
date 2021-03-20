af275a376eb43670ec2913ff0846d96aaadef019
Mehmet Secgin <mesecg@ttu.ee>
2021-03-20 16:44:47 +0300

    Used Lombok Getter and Setters instead of actually writing them down.

--- src/main/java/ee/taltech/calculator/dto/CalculationResult.java
+++ src/main/java/ee/taltech/calculator/dto/CalculationResult.java
@@ -1,7 +1,12 @@
 package ee.taltech.calculator.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class CalculationResult {
     private Integer min;
     private Float averageOfPositives;
@@ -9,34 +14,4 @@ public class CalculationResult {
     private Integer sumOfOdds;
     private List<Integer> powerOfFour;
 
-    public List<Integer> getPowerOfFour() {
-        return powerOfFour;
-    }
-    public void setPowerOfFour(List<Integer> powerOfFour) {
-        this.powerOfFour = powerOfFour;
-    }
-    public List<Integer> getOdds() {
-        return odds;
-    }
-    public void setOdds(List<Integer> odds) {
-        this.odds = odds;
-    }
-    public Integer getSumOfOdds() {
-        return sumOfOdds;
-    }
-    public void setSumOfOdds(Integer sumOfOdds) {
-        this.sumOfOdds = sumOfOdds;
-    }
-    public Integer getMin() {
-        return min;
-    }
-    public void setMin(Integer min) {
-        this.min = min;
-    }
-    public Float getAverageOfPositives() {
-        return averageOfPositives;
-    }
-    public void setAverageOfPositives(Float averageOfPositives) {
-        this.averageOfPositives = averageOfPositives;
-    }
 }
